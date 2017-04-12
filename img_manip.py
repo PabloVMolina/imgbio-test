d70348ac7486fda5eda6a4874065b41dd442b1c4
Rodolfo Ferro <rodolfoferroperez@gmail.com>
2017-04-12 14:43:39 -0500

    Updated manipulation example.

--- img_manip.py
+++ img_manip.py
@@ -1,8 +1,12 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-img = plt.imread("Lenna.png")
+def rgb2gray(rgb):
+    return rgb[...,:3] @ [0.299, 0.587, 0.114]
+
+img = plt.imread("Lenna.png",1)
 print(img.shape)
 
+plt.set_cmap('gray')
 plt.imshow(img)
 plt.show()
